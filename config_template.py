e4901addefe6e125f50799073d2d51a05cc35100
10kagan <49700749+10kagan@users.noreply.github.com>
2026-05-19 14:52:28 -0500

    Add files via upload

---
+++ config_template.py
@@ -0,0 +1,15 @@
+BASE_URL = "https://admin."
+BEARER_TOKEN = ""
+VIN = ""
+
+AUTH_ENDPOINT = "/api/client_token"
+API_KEY = "api key is here"
+APP_TYPE = "WEB_APP"
+
+CLIENT_ID = "client id is here"
+CLIENT_SECRET = "client secret is here"
+
+OEM_ID = "oem id is here"
+ACCOUNT_ID = "account id is here"
+
+REMOTE_DOOR_ENDPOINT = f"endpoint is here"
